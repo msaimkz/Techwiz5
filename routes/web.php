0af4e37571a4d0fa41610ec29f8dc638b66492dd
Fatimajameel439 <155413644+Fatimajameel439@users.noreply.github.com>
2024-09-18 17:36:49 +0500

    frontend

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
